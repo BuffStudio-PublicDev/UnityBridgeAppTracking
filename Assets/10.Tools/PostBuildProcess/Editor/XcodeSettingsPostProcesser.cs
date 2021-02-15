ff7796b52b377b11f4ab2b8a5f76e30fcc8c893a
ren <ren@buffstudio.com>
2021-02-15 12:02:27 +0900

    add PostProcessBuildScript for adding SKAdNetworkItems

--- Assets/10.Tools/PostBuildProcess/Editor/XcodeSettingsPostProcesser.cs
+++ Assets/10.Tools/PostBuildProcess/Editor/XcodeSettingsPostProcesser.cs
@@ -19,7 +19,7 @@ public class XcodeSettingsPostProcesser
     {
         DoAddAppTrackingFramework(path);
         DoAddNSUserTrackingUsageDescriptionToInfo(path);
-
+        DoAddSKAdNetworkItemsToInfo(path);
         NativeLocale.AddLocalizedStringsIOS(path, Path.Combine(Application.dataPath, "10.Tools/NativeLocale/iOS"));
     }
 
@@ -52,6 +52,8 @@ public class XcodeSettingsPostProcesser
             TextAsset textAsset = AssetDatabase.LoadAssetAtPath("Assets/10.Tools/AppTrackingTransparency/Editor/NSUserTrackingUsageDescription.txt", typeof(TextAsset)) as TextAsset;
             
             plistDoc.root.SetString(Key, textAsset.text);
+
+
             plistDoc.WriteToFile(infoPlistPath);
         }
         else
@@ -59,6 +61,83 @@ public class XcodeSettingsPostProcesser
             Debug.LogError("Error: Can't open " + infoPlistPath);
         }
 
+    }
+
+    private static void DoAddSKAdNetworkItemsToInfo(string projectPath)
+    {
+        string infoPlistPath = Path.Combine(projectPath, "Info.plist");
+
+        PlistDocument plistDoc = new PlistDocument();
+        plistDoc.ReadFromFile(infoPlistPath);
+        if (plistDoc.root != null)
+        {
+
+            PlistElementDict rootDict = plistDoc.root;
+
+            PlistElement elementSKAdNetworkItems = rootDict["SKAdNetworkItems"];
+
+            if (null == elementSKAdNetworkItems)
+            {
+                rootDict["SKAdNetworkItems"] = new PlistElementArray();
+
+                elementSKAdNetworkItems = rootDict["SKAdNetworkItems"];
+            }
+            else
+            {
+                PlistElementArray arrayAdNetworkItems = elementSKAdNetworkItems.AsArray();
+                int count = arrayAdNetworkItems.values.Count;
+                arrayAdNetworkItems.values.RemoveRange(0, count);
+            }
+
+            PlistElementArray arrayItems = elementSKAdNetworkItems.AsArray();
+
+            PlistElementArray addAdNetworks = DoGetAdNetworks();
+
+            for (int i = 0; i < addAdNetworks.values.Count; i++)
+            {
+                PlistElementDict item = addAdNetworks.values[i] as PlistElementDict;
+                arrayItems.values.Add(item);
+            }
+
+            plistDoc.WriteToFile(infoPlistPath);
+        }
+        else
+        {
+            Debug.LogError("Error: Can't open " + infoPlistPath);
+        }
+
+    }
+
+    private static PlistElementArray DoGetAdNetworks()
+    {
+
+        string adNetworksPlistPath = Path.Combine(DoGetProjectFolder(), "Assets/10.Tools/AppTrackingTransparency/Editor/SKAdNetworkItems.plist");
+
+        PlistDocument plistDoc = new PlistDocument();
+        plistDoc.ReadFromFile(adNetworksPlistPath);
+
+        PlistElement elementSKAdNetworkItems = plistDoc.root["SKAdNetworkItems"];
+
+        if (null == elementSKAdNetworkItems)
+        {
+            plistDoc.root["SKAdNetworkItems"] = new PlistElementArray();
+
+            elementSKAdNetworkItems = plistDoc.root["SKAdNetworkItems"];
+        }
+
+        PlistElementArray arrayItems = elementSKAdNetworkItems.AsArray();
+
+        return arrayItems;
+
+
+    }
+
+    private static string DoGetProjectFolder()
+    {
+        int position = Application.dataPath.IndexOf("/Assets");
+        string projectPath = Application.dataPath.Substring(0, position);
+
+        return projectPath;
     }
 
 }
